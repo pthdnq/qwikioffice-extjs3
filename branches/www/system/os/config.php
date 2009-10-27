bfbeb44f61200d8d755d4a4f028db4b298b3f9af
omenmm@gmail.com <omenmm@gmail.com@0bbe2212-c151-11de-bb5c-85734917f5ce>
2009-10-27 20:06:52 +0000

    ExtJs 3 url relocating

--- branches/www/system/os/config.php
+++ branches/www/system/os/config.php
@@ -48,10 +48,10 @@ class config {
 	 *
 	 */
 	public static function loadExtJs(){
-		$ext[]='<link rel="stylesheet" type="text/css" href="http://extjs.o/ext3/resources/css/ext-all.css" />';
-		$ext[]='<script src="http://extjs.o/ext3/adapter/ext/ext-base-debug.js"></script>';
-		$ext[]='<script src="http://extjs.o/ext3/ext-all-debug.js"></script>';
-		$ext[]='<script src="ext3/ext-fix.js"></script>';
+		$ext[]='<link rel="stylesheet" type="text/css" href="http://extjs.w.interia.pl/v3/resources/css/ext-all.css" />';
+		$ext[]='<script src="http://extjs.w.interia.pl/v3/adapter/ext/ext-base-debug.js"></script>';
+		$ext[]='<script src="http://extjs.w.interia.pl/v3/ext-all-debug.js"></script>';
+		$ext[]='<script src="/ext3/ext-fix.js"></script>';
 		return join("\n",$ext);
 	}
 }
